821019a8fca6b43406161adc29798a4f690000da
Keyman <518@msn.cn>
2023-06-04 11:21:31 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -5,20 +5,25 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "text/html;     charset=utf-8")
+func Defaulthandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 		fmt.Fprint(w, "客户端请求路径为 "+r.URL.Path)
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "你现在正在访问about目录1")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "你访问的页面没有找到")
 	}
-
 }
+
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "你现在正在访问about目录")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", Defaulthandler)
+	router.HandleFunc("/about", AboutHandler)
+	http.ListenAndServe(":3000", router)
 }
