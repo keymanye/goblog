5abef643d77b86fb5a77aafbe71acbcb58cd9105
Keyman <518@msn.cn>
2023-08-06 17:41:29 +0800

    读取请求数据

--- goblog/main.go
+++ goblog/main.go
@@ -33,7 +33,16 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	//if err := r.ParseForm(); err != nil {
+	//	fmt.Fprint(w, "请提供正确的数据！")
+	//	return
+	//}
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 body 的值为: %v <br>", r.FormValue("body"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
 
 func forceHTMLMiddleware(h http.Handler) http.Handler {
@@ -64,7 +73,7 @@ func articleCreateHandler(w http.ResponseWriter, r *http.Request) {
 			<title>创建文章 —— 我的技术博客</title>
 		</head>
 		<body>
-			<form action="%s" method="post">
+			<form action="%s?test=data" method="post">
 				<p><input type="text" name="title"></p>
 				<p><textarea name="body" cols="30" rows="10"></textarea></p>
 				<p><button type="submit">提交</button></p>
