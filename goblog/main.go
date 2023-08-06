83268013c9d517592a1a8fa9fb93d10bfb73e92d
Keyman <518@msn.cn>
2023-08-06 19:55:05 +0800

    表单验证

--- goblog/main.go
+++ goblog/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
 )
+
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
 
 var router = mux.NewRouter().StrictSlash(true)
 
@@ -33,16 +41,67 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	//if err := r.ParseForm(); err != nil {
-	//	fmt.Fprint(w, "请提供正确的数据！")
-	//	return
-	//}
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+	errors := make(map[string]string)
+	//验证title内容
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if len(title) < 3 || len(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
+	}
+	//验证body内容
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if len(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+	// 检查是否有错误
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过!<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title)
+		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	} else {
+		html := `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<title>创建文章 —— 我的技术博客</title>
+				<style type="text/css">.error {color: red;}</style>
+			</head>
+			<body>
+				<form action="{{ .URL }}" method="post">
+					<p><input type="text" name="title" value="{{ .Title }}"></p>
+					{{ with .Errors.title }}
+					<p class="error">{{ . }}</p>
+					{{ end }}
+					<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+					{{ with .Errors.body }}
+					<p class="error">{{ . }}</p>
+					{{ end }}
+					<p><button type="submit">提交</button></p>
+				</form>
+			</body>
+			</html>
+			`
+		storeURL, _ := router.Get("articles.store").URL()
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		templ, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+		if err = templ.Execute(w, data); err != nil {
+			panic(err)
+		}
 
-	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
-	fmt.Fprintf(w, "r.Form 中 body 的值为: %v <br>", r.FormValue("body"))
-	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
-	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
-	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+	}
 }
 
 func forceHTMLMiddleware(h http.Handler) http.Handler {
